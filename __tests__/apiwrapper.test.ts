ed878a8fc16105987ce10432e4fe97becdcf5825
arsalan.ahmed <arsalanabc@gmail.com>
2021-11-14 13:14:57 -0500

    api tests passing

--- __tests__/apiwrapper.test.ts
+++ __tests__/apiwrapper.test.ts
@@ -1,7 +1,8 @@
+// import { assert } from 'chai';
 import ApiWrapper from '../src/ApiWrapper';
 
 describe('Api Wrapper', () => {
-  const API_URL = `https://openweathermap.org/api`;
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather`;
   const API_KEY = `cf002751564a4c78f5f7ed479f1b9ba3`;
 
   const apiWrapper: ApiWrapper = new ApiWrapper(API_URL, API_KEY);
@@ -10,9 +11,26 @@ describe('Api Wrapper', () => {
     const CITY = 'Montreal';
     const cityWeather = await apiWrapper.weatherByCity(CITY);
 
-    console.log(cityWeather);
-
     expect(cityWeather).not.toBeNull;
     expect(cityWeather).toHaveProperty('name');
+    expect(cityWeather.name).toEqual(CITY);
+  });
+
+  it('should call Weather by zip', async () => {
+    const zipNY = '10001';
+    const cityWeather = await apiWrapper.weatherByZip(zipNY);
+
+    expect(cityWeather).not.toBeNull;
+    expect(cityWeather.name).toEqual('New York');
+  });
+
+  it('should call Weather by coordinates', async () => {
+    const lon = -122.08;
+    const lat = 37.39;
+    const cityWeather = await apiWrapper.weatherByCoordinates(lat, lon);
+
+    expect(cityWeather).not.toBeNull;
+    expect(cityWeather).toHaveProperty('coord');
+    expect(cityWeather.coord).toEqual({ lat, lon });
   });
 });
--- src/ApiWrapper.ts
+++ src/ApiWrapper.ts
@@ -1,30 +1,32 @@
-require('regenerator-runtime/runtime');
-import fetch from 'node-fetch';
+import fetch from 'cross-fetch';
 
 class ApiWrapper {
   API: string;
 
   constructor(url: string, key: string) {
-    this.API = `${url}/appid=${key}`;
+    this.API = `${url}?appid=${key}`;
   }
   async weatherByCity(cityName: string) {
-    const weatherByCity = await this.fetchCall(cityName);
-    return weatherByCity;
+    return await this.fetchCall(`q=${cityName}`);
   }
 
-  weatherByZip(cityName: string) {
-    return cityName;
+  async weatherByZip(zip: string) {
+    return await this.fetchCall(`zip=${zip}`);
   }
 
-  weatherByCoordinates(lat: number, long: number) {
-    console.log(lat, long);
-    return '';
+  async weatherByCoordinates(lat: number, long: number) {
+    return await this.fetchCall(`lat=${lat}&lon=${long}`);
   }
 
   private async fetchCall(q: string) {
-    const response = await fetch(`${this.API}&q=${q}`);
-    const data = await response.json();
-    return data;
+    try {
+      const response = await fetch(`${this.API}&${q}`);
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      console.log('error', error);
+    }
   }
 }
 
