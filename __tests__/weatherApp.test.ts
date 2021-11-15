e152d044b0c4fda90916ba5b01e6fbd95ed6297c
arsalan.ahmed <arsalanabc@gmail.com>
2021-11-14 19:53:32 -0500

    use mock for api wrapper

--- __tests__/weatherApp.test.ts
+++ __tests__/weatherApp.test.ts
@@ -1,36 +1,30 @@
 import ApiWrapper from '../src/ApiWrapper';
 import WeatherApp from '../src/WeatherApp';
 
+jest.mock('../src/ApiWrapper.ts');
+
 describe('Weather App', () => {
-  const apiWrapper: ApiWrapper = new ApiWrapper('asd', 'sd');
-  const weatherByCitySpy = jest.spyOn(apiWrapper, 'weatherByCity');
-  const weatherByZipSpy = jest.spyOn(apiWrapper, 'weatherByZip');
-  const weatherByGeoSpy = jest.spyOn(apiWrapper, 'weatherByCoordinates');
-
+  const apiWrapper = new ApiWrapper('dummy_url', 'dummy_key');
   const wApp = new WeatherApp(apiWrapper);
-
-  // it('should create a WeatherApp instance', () => {
-  //  expect(typeof wApp).toBe(WeatherApp);
-  // }) ;
 
   it('should call Weather by city in the wrapper', () => {
     wApp.getWeatherByCity('Montreal');
 
-    expect(weatherByCitySpy).toHaveBeenCalledTimes(1);
-    expect(weatherByCitySpy).toHaveBeenCalledWith('Montreal');
+    expect(apiWrapper.weatherByCity).toHaveBeenCalledTimes(1);
+    expect(apiWrapper.weatherByCity).toHaveBeenCalledWith('Montreal');
   });
 
   it('should call Weather by Zip in the wrapper', () => {
     wApp.getWeatherByZipCode('12345');
 
-    expect(weatherByZipSpy).toBeCalledTimes(1);
-    expect(weatherByZipSpy).toHaveBeenCalledWith('12345');
+    expect(apiWrapper.weatherByZip).toBeCalledTimes(1);
+    expect(apiWrapper.weatherByZip).toHaveBeenCalledWith('12345');
   });
 
   it('should call Weather by GEO coordinates in the wrapper', () => {
     wApp.getWeatherByCoordinates(1, 2);
 
-    expect(weatherByGeoSpy).toBeCalledTimes(1);
-    expect(weatherByGeoSpy).toHaveBeenCalledWith(1, 2);
+    expect(apiWrapper.weatherByCoordinates).toBeCalledTimes(1);
+    expect(apiWrapper.weatherByCoordinates).toHaveBeenCalledWith(1, 2);
   });
 });
